7978f2530c42457fb54708b885f13a5c7d9a5eb5
Gianmaria <gianmaria.bonici@gmail.com>
2024-12-22 13:25:38 +0100

    do not echo, set title and prompt

--- env/dev_env.bat
+++ env/dev_env.bat
@@ -1,34 +1,20 @@
-@echo off 
+@echo off
 
 ::echo Setting up Qt
 ::call qt_env.bat
 
-echo Setting up RadDbg
 call raddbg_env.bat
 
-echo Setting up Rust
 call rust_env.bat
-
-echo Setting up Zig
 call zig_env.bat
-
-echo Setting up Go
 call go_env.bat
-
-echo Setting up c3
 call c3_env.bat
-
-echo Setting up java
 call java_env.bat
 
-echo Setting up VS
 call vs_env.bat
-
-echo Setting up Cmake
 call cmake_env.bat
-
-echo Setting up Ninja
 call ninja_env.bat
-
-echo Setting up Python
 call python_env.bat
+
+title Dev env
+set PROMPT=[Dev] $P$_$$$S
