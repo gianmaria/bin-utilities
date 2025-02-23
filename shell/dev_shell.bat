4283d01357ea470f5063cac927f876814969aa0f
Gianmaria <gianmaria.bonici@gmail.com>
2025-02-23 18:28:29 +0100

    only one dev_shell calling custom_vs22_env.bat

--- shell/dev_shell.bat
+++ shell/dev_shell.bat
@@ -2,23 +2,26 @@
 
 setlocal
 
+::echo Setting up Qt
+::call qt_env.bat
+
 call raddbg_env.bat
 
-:: call java_env.bat
-:: call go_env.bat
-:: call perl_env.bat
-:: call rust_env.bat
-:: call zig_env.bat
+call rust_env.bat
+call zig_env.bat
+call go_env.bat
+call c3_env.bat
+call java_env.bat
 
-call qt_env.bat
-
+call custom_vs22_env.bat
+call cmake_env.bat
 call ninja_env.bat
 call python_env.bat
-call cmake_env.bat
-call clang_env.bat
-call vs_env.bat
+:: call meson_env.bat
 
-start "** dev shell **" /D C:\Workspace cmd
+title Dev env
+set PROMPT=[Dev] $P$_$$$S
+
+start "** Dev env **" /D C:\Workspace cmd
+
 endlocal
-
-exit
