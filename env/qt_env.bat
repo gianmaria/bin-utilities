6cbc8a62d3af6351f66c097f27929a0e9aa51f14
Gianmaria <gianmaria.bonici@gmail.com>
2025-02-23 18:27:35 +0100

    update qt

--- env/qt_env.bat
+++ env/qt_env.bat
@@ -4,8 +4,8 @@ set PATH=C:\Qt\Tools\OpenSSLv3\Win_x64\bin;%PATH%
 
 :: QtCreator
 set PATH=C:\Qt\Tools\QtCreator\bin;%PATH%
-set PATH=C:\Qt\Tools\QtCreator\bin\clang\bin;%PATH%
-set PATH=C:\Qt\Tools\QtCreator\bin\jom;%PATH%
+:: set PATH=C:\Qt\Tools\QtCreator\bin\clang\bin;%PATH%
+:: set PATH=C:\Qt\Tools\QtCreator\bin\jom;%PATH%
 
 :: Qt libs
-set PATH=C:\Qt\6.7.0\msvc2019_64\bin;%PATH%
+set PATH=C:\Qt\6.8.1\msvc2022_64\bin;%PATH%
